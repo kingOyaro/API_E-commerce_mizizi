19cef3fa95b9028b2c8e70f22e35070e3c75d34b
BlackpearlJack <lestercarlyle@gmail.com>
2020-09-23 11:54:38 +0300

    latest updates

---
+++ app/Http/Controllers/ShopController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Product;
+use Illuminate\Http\Request;
+
+class ShopController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $products = Product::inRandomOrder()->take(12)->get();
+
+        return view('shop')->with('products', $products);
+    }
+
+}
