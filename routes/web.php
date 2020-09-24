9144f8918d55f69aad10d16d9a41fb749026756d
kingOyaro <dalton.oyaro@strathmore.edu>
2020-09-24 21:24:39 +0300

    cart

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,15 @@
 */
 
 Route::get('/', 'landingPageController@index')->name('landing-page');
+
 Route::get('/shop', 'ShopController@index')->name('shop.index');
 Route::get('/shop/{product}', 'ShopController@show')->name('shop.show');
 
+Route::get('/cart', 'CartController@index')->name('cart.index');
+
+Route::get('/search', 'ShopController@search')->name('search');
+
+Route::get('/search-algolia', 'ShopController@searchAlgolia')->name('search-algolia');
+
+Route::view('/checkout', 'checkout');
+Route::view('/thankyou', 'thankyou');
