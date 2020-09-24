5fef26cc0fc6dd278ab842157d229abf842f04f9
kingOyaro <dalton.oyaro@strathmore.edu>
2020-09-24 14:06:29 +0300

    added footer

--- resources/views/landing-page.blade.php
+++ resources/views/landing-page.blade.php
@@ -95,6 +95,18 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div><!-- end blog-section -->
+    <footer>
+        <div class="footer-content container">
+            <div class="made-with">Made with <i class="fa fa-heart"></i> by Vikendi_Noobs</div>
+            <ul>
+                <li>Follow Us:</li>
+                <li><a href="#"><i class="fa fa-globe"></i></a></li>
+                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+                <li><a href="#"><i class="fa fa-youtube"></i></a></li>
+            </ul>
+        </div><!-- end footer-content-->
+    </footer>
 </body>
 </html>
