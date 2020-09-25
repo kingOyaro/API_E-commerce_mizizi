81d227c8bf89513dd9ae1672340f6af42058ec7d
ShitsukaneJS <jeffrey.shitsukane@strathmore.edu>
2020-09-25 18:59:47 +0300

    commit

---
+++ database/seeds/CategoriesTableSeeder.php
@@ -0,0 +1,48 @@
+<?php
+
+use App\Category;
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+
+class CategoriesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $category = Category::firstOrNew([
+            'slug' => 'category-1',
+        ]);
+        if (!$category->exists) {
+            $category->fill([
+                'name' => 'Category 1',
+            ])->save();
+        }
+
+        $category = Category::firstOrNew([
+            'slug' => 'category-2',
+        ]);
+        if (!$category->exists) {
+            $category->fill([
+                'name' => 'Category 2',
+            ])->save();
+        }
+
+        $now = Carbon::now()->toDateTimeString();
+
+        Category::insert([
+            ['name' => 'Laptops', 'slug' => 'laptops', 'created_at' => $now, 'updated_at' => $now],
+            ['name' => 'Desktops', 'slug' => 'desktops', 'created_at' => $now, 'updated_at' => $now],
+            ['name' => 'Mobile Phones', 'slug' => 'mobile-phones', 'created_at' => $now, 'updated_at' => $now],
+            ['name' => 'Tablets', 'slug' => 'tablets', 'created_at' => $now, 'updated_at' => $now],
+            ['name' => 'TVs', 'slug' => 'tvs', 'created_at' => $now, 'updated_at' => $now],
+            ['name' => 'Digital Cameras', 'slug' => 'digital-cameras', 'created_at' => $now, 'updated_at' => $now],
+            ['name' => 'Appliances', 'slug' => 'appliances', 'created_at' => $now, 'updated_at' => $now],
+
+
+        ]);
+    }
+}
