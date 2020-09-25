fef9cfea9cccb5dff335fb4b2f5a8ce7cc3c1330
kingOyaro <dalton.oyaro@strathmore.edu>
2020-09-25 14:25:18 +0300

    presntPrice

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -100,7 +100,7 @@
                     @foreach (Cart::content() as $item)
                         <div class="checkout-table-row">
                             <div class="checkout-table-row-left">
-                                <img src="{{-- productImage($item->model->image) --}}" alt="item" class="checkout-table-img">
+                                <a href="{{ route('shop.show', $item->model->slug) }}"><img src="{{ asset('img/products/'.$item->model->slug.'.jpg') }}" alt="item" class="checkout-table-img"></a>
                                 <div class="checkout-item-details">
                                     <div class="checkout-table-item">{{ $item->model->name }}</div>
                                     <div class="checkout-table-description">{{ $item->model->details }}</div>
